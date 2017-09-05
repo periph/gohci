a8622d95e6c4debac8063eece6b1114d063d36ed
Marc-Antoine Ruel <maruel@chromium.org>
2017-09-04 21:58:15 -0400

    Add required import part as periph.io/x/gohci

---
+++ lib/lib.go
@@ -0,0 +1,8 @@
+// Copyright 2017 Marc-Antoine Ruel. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+// Package lib exposes SetConsoleTitle.
+//
+// This is mainly useful on Windows.
+package lib // import "periph.io/x/gohci/lib"
