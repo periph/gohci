0ee8b04dd64eb7c523d5ce7de84404750c6b40ca
Marc-Antoine Ruel <maruel@chromium.org>
2018-03-11 17:20:08 -0400

    Update vendor

--- update_vendor.sh
+++ update_vendor.sh
@@ -9,8 +9,8 @@ go get -v github.com/golang/dep
 dep ensure -update
 
 # Trim google.golang.org dependency. 
-rm vendor/golang.org/x/oauth2/client_appengine.go
+rm vendor/golang.org/x/oauth2/internal/client_appengine.go
 rm -rf vendor/google.golang.org/
 # TODO(maruel): Update Gopkg.lock to remove reference to appengine.
 
-git add .
+git add -f .
